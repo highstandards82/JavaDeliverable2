e4b96e0891c3eaed985f26b6702b00e50ea4971f
highstandards82 <45728426+highstandards82@users.noreply.github.com>
2018-12-13 10:03:17 -0500

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Application {
+
+	public static void main(String[] args){
+		
+	
+		if(eventTypeCasual) {
+		  System.out.println("The event is casual");
+		}
+		 else if(partySize + 1 + i++) {
+			 System.out.println("The party size is 1 or more");
+		}
+			 
+		 else if(mealSandwiches) {
+			 System.out.println("The meal suggestion are sandwiches ");
+		}
+	     else if(prepMicrowave) {
+			 System.out.println("Sandwiches prepared in microwave");
+	    
+	     }	 
+	     else if(eventTypeFormal) {
+	    	 System.out.println("The event type is formal");
+	    }   
+	     else if(partySize + 2 + i++) {
+			 System.out.println("The party size is 2 or more");
+		}
+		 else if(mealFriedChicken) {
+			 System.out.println("The meal suggestion Fried Chicken");
+		}
+	     else if(prepKitchen) {
+			 System.out.println("Sandwiches prepared in kitchen");
+	    }	 
+	     
+	     else if(eventTypeSemiFormal) {
+	    	 System.out.println("The event type is semi-formal");
+	    }   
+	     else if(partySize + 13 + i++) {
+			 System.out.println("The party size is 13 or more");
+		}
+			 
+		 else if (mealChickenparmesan) {
+			 System.out.println("The meal suggestion is chicken parmesan ");
+		}
+	     else if(prepCaterer) {
+			 System.out.println("Chicken parmesan prepared by a caterer");
+	    }	 
+	     
+	}
